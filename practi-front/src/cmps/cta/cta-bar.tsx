da684b4491deccfcf86fe857263fda5d5cf9e62f
nitay510 <nitay510@gmail.com>
2023-12-27 10:08:26 +0200

    try to fix bugs with the cta-baar

--- practi-front/src/cmps/cta/cta-bar.tsx
+++ practi-front/src/cmps/cta/cta-bar.tsx
@@ -1,39 +1,45 @@
-import { MdHistory } from 'react-icons/md';
-import { MdPeople } from 'react-icons/md';
-import { MdHome } from 'react-icons/md';
-import { Link, useNavigate } from 'react-router-dom';
+import { MdHistory, MdPeople, MdHome } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-
-/* this component is the down menu of the app, click on each icon move you to another place */
 
 export const CtaBar = (): JSX.Element => {
   const navigate = useNavigate();
-  const [activeLink, setActiveLink] = useState<string>('https://practi-web.onrender.com');
+  const [activeIcon, setActiveIcon] = useState<string>('');
 
-  const handleNavigation = (path: string) => {
-    setActiveLink(path);
-    navigate(path);
+  const handleAppNavigation = () => {
+    setActiveIcon('app');
+    navigate('/app');
+  };
+
+  const handleHistoryNavigation = () => {
+    setActiveIcon('history');
+    navigate('/history');
+  };
+
+  const handleHistoryFriendsNavigation = () => {
+    setActiveIcon('historyFriends');
+    navigate('/historyFriends');
   };
 
   return (
     <div className="cta-bar">
       <div
-        onClick={() => handleNavigation('/app')}
-        className={`ic-container ${activeLink === '/app' ? 'ic-active' : ''}`}
+        onClick={handleAppNavigation}
+        className={`ic-container ${activeIcon === 'app' ? 'ic-active' : ''}`}
       >
         <MdHome />
       </div>
 
       <div
-        onClick={() => handleNavigation('/history')}
-        className={`ic-container ${activeLink === '/history' ? 'ic-active' : ''}`}
+        onClick={handleHistoryNavigation}
+        className={`ic-container ${activeIcon === 'history' ? 'ic-active' : ''}`}
       >
         <MdHistory />
       </div>
 
       <div
-        onClick={() => handleNavigation('/historyFriends')}
-        className={`ic-container ${activeLink === '/historyFriends' ? 'ic-active' : ''}`}
+        onClick={handleHistoryFriendsNavigation}
+        className={`ic-container ${activeIcon === 'historyFriends' ? 'ic-active' : ''}`}
       >
         <MdPeople />
       </div>
