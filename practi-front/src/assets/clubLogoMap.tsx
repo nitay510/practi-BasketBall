da058449ddb451c382254bf698638e0328ab0514
nitay510 <nitay510@gmail.com>
2024-09-24 08:08:40 +0300

    try using png

--- practi-front/src/assets/clubLogoMap.tsx
+++ practi-front/src/assets/clubLogoMap.tsx
@@ -1,6 +1,6 @@
 // clubLogoMap.js
 export const clubLogoMap: { [key: string]: string } = {
-    "הפועל משגב": "misgav.jpg",
+    "הפועל משגב": "misgav.png",
     "הפועל תל אביב": "hapoel_tel_aviv.jpg",
     // Add more club mappings here...
   };
