03fa20002b49c5e5630163b43d677716b8920446
nitay510 <nitay510@gmail.com>
2024-10-17 23:44:29 +0300

    just change build

--- build/firebase-messaging-sw.js
+++ build/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
-importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging.js');
+importScripts('/firebase-app.js');
+importScripts('/firebase-messaging.js');
 
 // Initialize Firebase in the service worker
-console.log('[firebase-messaging-sw.js] Starting Firebase initialization...');
+console.log('[firebase-messaging-sw.js] Initializing Firebase...');
 firebase.initializeApp({
   apiKey: 'BPVVffu9hkSGUvIQ2j12xoaVcAHc9C4da3ybDGpha0HPKMoT6q_tjITl-ekDBfL387vXZqxEzbbFuGi9MIZcAvg',
   authDomain: 'practi-project.firebaseapp.com',
@@ -13,7 +13,6 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-console.log('[firebase-messaging-sw.js] Firebase initialized successfully.');
 
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message: ', payload);
@@ -21,9 +20,6 @@ messaging.onBackgroundMessage((payload) => {
   const notificationOptions = {
     body: payload.notification.body,
   };
-  
-  console.log('[firebase-messaging-sw.js] Displaying notification: ', notificationTitle, notificationOptions);
+
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
-
-console.log('[firebase-messaging-sw.js] Service worker setup complete.');
